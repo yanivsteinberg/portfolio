cb0ccf3b56078db53f13d03ab4637f8d8417d074
Yaniv Steinberg <yaniv.steinberg@mail.huji.ac.il>
2020-08-03 16:34:12 +0300

    contact form formspree

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -162,32 +162,40 @@ $(document).ready(function () {
   $('.submit').click(function (event) {
     console.log('clicked button')
 
+    var fullName = $('.fullName').val()
     var email = $('.email').val()
     var subject = $('.subject').val()
     var message = $('.message').val()
-    var statusElm = $('.status')
-    statusElm.empty()
+    var statusElm = $('.status').val()
+    statusElm.empty();
+
+    if (fullName.length > 2) {
+      statusElm.append('<div>Name is valid</div>')
+    } else {
+      event.preventDefault()
+      statusElm.append('<div>Name is not valid</div>')
+    };
 
     if (email.length > 5 && email.includes('@') && email.includes('.')) {
       statusElm.append('<div>Email is valid</div>')
     } else {
       event.preventDefault()
       statusElm.append('<div>Email is not valid</div>')
-    }
+    };
 
     if (subject.length >= 2) {
       statusElm.append('<div>Subject is valid</div>')
     } else {
       event.preventDefault()
       statusElm.append('<div>Subject is not valid</div>')
-    }
+    };
 
-    if (message.length >= 20) {
+    if (message.length >= 10) {
       statusElm.append('<div>Message is valid</div>')
     } else {
       event.preventDefault()
       statusElm.append('<div>Message is not valid</div>')
-    }
+    };
 
   })
-})
+});
